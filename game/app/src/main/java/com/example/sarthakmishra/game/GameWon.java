0bafefd5654b30aec6ea7e385cb6787c16e40d83
sarthaksarm <sarmsarthak@gmail.com>
2020-03-20 12:44:44 +0530

    CodeBase

---
+++ game/app/src/main/java/com/example/sarthakmishra/game/GameWon.java
@@ -0,0 +1,28 @@
+package com.example.sarthakmishra.game;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GameWon extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_won);
+    }
+
+    //This is onclick listener for button
+    //it will navigate from this activity to MainGameActivity
+    public void PlayAgain(View view) {
+        Intent intent = new Intent(GameWon.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+}
